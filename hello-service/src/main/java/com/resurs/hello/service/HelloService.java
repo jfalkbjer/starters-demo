4a1de7fadff0d0986ea3080d21e622f04ec91655
Jimmy Falkbjer <Jimmy.Falkbjer@resurs.se>
2017-12-01 14:19:52 +0100

    Extracted service to own jar.

--- hello-service/src/main/java/com/resurs/hello/service/HelloService.java
+++ hello-service/src/main/java/com/resurs/hello/service/HelloService.java
@@ -4,10 +4,6 @@ import com.resurs.hello.service.model.Salutation;
 
 import lombok.RequiredArgsConstructor;
 
-/**
- * Service is tightly coupled to the Hello Application. Your are not able to
- * reuse the HelloService in another project.
- */
 @RequiredArgsConstructor
 public class HelloService {
     private final String salutationPrefix;
